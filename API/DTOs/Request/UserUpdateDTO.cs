2e401aa84e4dae2600e60540f66168cea28963eb
Minh Đăng <91261081+minhdang0603@users.noreply.github.com>
2025-06-09 16:47:16 +0700

    minor update

--- API/DTOs/Request/UserUpdateDTO.cs
+++ API/DTOs/Request/UserUpdateDTO.cs
@@ -2,9 +2,9 @@ namespace API.DTOs.Request
 {
     public class UserUpdateDTO
     {
-        public string UserId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? UserId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
--- API/DTOs/Response/LoginResponseDTO.cs
+++ API/DTOs/Response/LoginResponseDTO.cs
@@ -2,7 +2,7 @@ namespace API.DTOs.Response;
 
 public class LoginResponseDTO
 {
-    public string Token { get; set; }
+    public string? Token { get; set; }
     public DateTime Expiration { get; set; }
-    public UserDTO User { get; set; }
+    public UserDTO? User { get; set; }
 }
