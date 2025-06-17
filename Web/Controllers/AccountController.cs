b81dab21138d364f2a6bc2ea6901d1aa4e9855d6
vuhuyanhnew <vuhuyanhnew@gmail.com>
2025-06-17 20:12:15 +0700

    add homepage

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
