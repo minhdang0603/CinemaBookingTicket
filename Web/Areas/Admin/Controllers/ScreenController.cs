0feec36b46bae39e7ac0e675aae65327af34a3a4
Minh Đăng <91261081+minhdang0603@users.noreply.github.com>
2025-06-20 11:52:31 +0700

    modified admin ui and home ui

--- Web/Areas/Admin/Controllers/ScreenController.cs
+++ Web/Areas/Admin/Controllers/ScreenController.cs
@@ -1,18 +1,15 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
 using Utility;
-using Web.Models;
 
 namespace Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     // [Authorize(Roles = Constant.Role_Admin)]
-    public class MovieController : Controller
+    public class ScreenController : Controller
     {
         public IActionResult Index()
         {
-
             return View();
         }
 
