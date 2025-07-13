dfd55d241398c46c633406fcddf6c37ad619c441
vuhuyanhnew <vuhuyanhnew@gmail.com>
2025-07-13 21:53:46 +0700

    update view history

---
+++ Web/Areas/Customer/Controllers/HistoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+
+    public class HistoryController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
