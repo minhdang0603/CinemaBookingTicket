48bf6057cd9699bab36a57701ca08cdb55597f4a
Minh Đăng <91261081+minhdang0603@users.noreply.github.com>
2025-07-18 23:12:15 +0700

    Add Avatar 3 to seed data and update movie status usage

--- API/Data/DbInitializer/DbInitializer.cs
+++ API/Data/DbInitializer/DbInitializer.cs
@@ -87,76 +87,89 @@ namespace API.Data.DbInitializer
             }
 
             // Add sample movies
-            var movies = new List<Movie>
-    {
-        new Movie
-        {
-            Title = "Avengers: Endgame",
-            Description = "Cuộc chiến cuối cùng của các siêu anh hùng Marvel",
-            Director = "Anthony Russo, Joe Russo",
-            Cast = "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
-            Duration = 181,
-            ReleaseDate = new DateOnly(2024, 6, 15),
-            AgeRating = "PG-13",
-            Status = "NowShowing",
-            PosterUrl = "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-            IsActive = true
-        },
-        new Movie
-        {
-            Title = "Spider-Man: No Way Home",
-            Description = "Peter Parker phải đối mặt với các phản diện từ đa vũ trụ",
-            Director = "Jon Watts",
-            Cast = "Tom Holland, Zendaya, Benedict Cumberbatch",
-            Duration = 148,
-            ReleaseDate = new DateOnly(2024, 7, 1),
-            AgeRating = "PG-13",
-            Status = "NowShowing",
-            PosterUrl = "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_.jpg",
-            IsActive = true
-        },
-        new Movie
-        {
-            Title = "The Lion King",
-            Description = "Câu chuyện về chú sư tử Simba và hành trình trở thành vua",
-            Director = "Jon Favreau",
-            Cast = "Donald Glover, Beyoncé, James Earl Jones",
-            Duration = 118,
-            ReleaseDate = new DateOnly(2024, 8, 10),
-            AgeRating = "G",
-            Status = "NowShowing",
-            PosterUrl = "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_.jpg",
-            IsActive = true
-        },
-        new Movie
-        {
-            Title = "Fast & Furious 10",
-            Description = "Gia đình Dom Toretto tiếp tục cuộc phiêu lưu tốc độ",
-            Director = "Louis Leterrier",
-            Cast = "Vin Diesel, Michelle Rodriguez, Tyrese Gibson",
-            Duration = 141,
-            ReleaseDate = new DateOnly(2024, 5, 20),
-            AgeRating = "PG-13",
-            Status = "NowShowing",
-            PosterUrl = "https://m.media-amazon.com/images/M/MV5BNzVlYzk0NDMtNjgxYy00NmZhLWIwMDQtZDg5NjQ4YjkzMzQzXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
-            IsActive = true
-        },
-        new Movie
-        {
-            Title = "The Conjuring 4",
-            Description = "Ed và Lorraine Warren tiếp tục cuộc chiến với các thế lực siêu nhiên",
-            Director = "Michael Chaves",
-            Cast = "Patrick Wilson, Vera Farmiga",
-            Duration = 112,
-            ReleaseDate = new DateOnly(2024, 9, 5),
-            AgeRating = "R",
-            Status = "NowShowing",
-            PosterUrl = "https://m.media-amazon.com/images/M/MV5BMDU2NjIyOGYtMGFkYS00ZTk0LWJjMjMtNTdkZTdlZGUzNzg5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
-            IsActive = true
-        }
-    };
+	    var movies = new List<Movie>
+            {
+	            new Movie
+	            {
+		            Title = "Avengers: Endgame",
+		            Description = "Cuộc chiến cuối cùng của các siêu anh hùng Marvel",
+		            Director = "Anthony Russo, Joe Russo",
+		            Cast = "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
+		            Duration = 181,
+		            ReleaseDate = new DateOnly(2024, 6, 15),
+		            AgeRating = "PG-13",
+		            Status = Constant.Movie_Status_NowShowing,
+		            PosterUrl = "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
+		            IsActive = true
+	            },
+	            new Movie
+	            {
+		            Title = "Spider-Man: No Way Home",
+		            Description = "Peter Parker phải đối mặt với các phản diện từ đa vũ trụ",
+		            Director = "Jon Watts",
+		            Cast = "Tom Holland, Zendaya, Benedict Cumberbatch",
+		            Duration = 148,
+		            ReleaseDate = new DateOnly(2024, 7, 1),
+		            AgeRating = "PG-13",
+		            Status = Constant.Movie_Status_NowShowing,
+		            PosterUrl = "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_.jpg",
+		            IsActive = true
+	            },
+	            new Movie
+	            {
+		            Title = "The Lion King",
+		            Description = "Câu chuyện về chú sư tử Simba và hành trình trở thành vua",
+		            Director = "Jon Favreau",
+		            Cast = "Donald Glover, Beyoncé, James Earl Jones",
+		            Duration = 118,
+		            ReleaseDate = new DateOnly(2024, 8, 10),
+		            AgeRating = "G",
+		            Status = Constant.Movie_Status_NowShowing,
+		            PosterUrl = "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_.jpg",
+		            IsActive = true
+	            },
+	            new Movie
+	            {
+		            Title = "Fast & Furious 10",
+		            Description = "Gia đình Dom Toretto tiếp tục cuộc phiêu lưu tốc độ",
+		            Director = "Louis Leterrier",
+		            Cast = "Vin Diesel, Michelle Rodriguez, Tyrese Gibson",
+		            Duration = 141,
+		            ReleaseDate = new DateOnly(2024, 5, 20),
+		            AgeRating = "PG-13",
+		            Status = Constant.Movie_Status_NowShowing,
+		            PosterUrl = "https://m.media-amazon.com/images/M/MV5BNzVlYzk0NDMtNjgxYy00NmZhLWIwMDQtZDg5NjQ4YjkzMzQzXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
+		            IsActive = true
+	            },
+	            new Movie
+	            {
+		            Title = "The Conjuring 4",
+		            Description = "Ed và Lorraine Warren tiếp tục cuộc chiến với các thế lực siêu nhiên",
+		            Director = "Michael Chaves",
+		            Cast = "Patrick Wilson, Vera Farmiga",
+		            Duration = 112,
+		            ReleaseDate = new DateOnly(2024, 9, 5),
+		            AgeRating = "R",
+		            Status = Constant.Movie_Status_NowShowing,
+		            PosterUrl = "https://m.media-amazon.com/images/M/MV5BMDU2NjIyOGYtMGFkYS00ZTk0LWJjMjMtNTdkZTdlZGUzNzg5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+		            IsActive = true
+	            },
+	            new Movie
+	            {
+		            Title = "Avatar 3",
+		            Description = "Phần tiếp theo của Avatar",
+		            Director = "James Cameron",
+		            Cast = "Sam Worthington, Zoe Saldana",
+		            Duration = 120,
+		            ReleaseDate = new DateOnly(2025, 12, 15),
+		            AgeRating = "PG-13",
+		            Status = Constant.Movie_Status_ComingSoon,
+		            PosterUrl = "https://m.media-amazon.com/images/M/MV5BYjE0OWZmYWMtZjBhMi00YzM5LTkzOTctOTZhMTIwNDcxY2U0XkEyXkFqcGc@._V1_.jpg",
+		            IsActive = true
+	            }
+            };
 
-            _context.Movies.AddRange(movies);
+			_context.Movies.AddRange(movies);
             _context.SaveChanges();
 
             // Link movies with genres
@@ -166,15 +179,15 @@ namespace API.Data.DbInitializer
             var horrorGenre = _context.Genres.First(g => g.Name == "Kinh dị");
 
             var movieGenres = new List<MovieGenre>
-    {
-        new MovieGenre { MovieId = movies[0].Id, GenreId = actionGenre.Id },
-        new MovieGenre { MovieId = movies[0].Id, GenreId = sciFiGenre.Id },
-        new MovieGenre { MovieId = movies[1].Id, GenreId = actionGenre.Id },
-        new MovieGenre { MovieId = movies[1].Id, GenreId = sciFiGenre.Id },
-        new MovieGenre { MovieId = movies[2].Id, GenreId = animationGenre.Id },
-        new MovieGenre { MovieId = movies[3].Id, GenreId = actionGenre.Id },
-        new MovieGenre { MovieId = movies[4].Id, GenreId = horrorGenre.Id }
-    };
+            {
+                new MovieGenre { MovieId = movies[0].Id, GenreId = actionGenre.Id },
+                new MovieGenre { MovieId = movies[0].Id, GenreId = sciFiGenre.Id },
+                new MovieGenre { MovieId = movies[1].Id, GenreId = actionGenre.Id },
+                new MovieGenre { MovieId = movies[1].Id, GenreId = sciFiGenre.Id },
+                new MovieGenre { MovieId = movies[2].Id, GenreId = animationGenre.Id },
+                new MovieGenre { MovieId = movies[3].Id, GenreId = actionGenre.Id },
+                new MovieGenre { MovieId = movies[4].Id, GenreId = horrorGenre.Id }
+            };
 
             _context.MovieGenres.AddRange(movieGenres);
             _context.SaveChanges();
